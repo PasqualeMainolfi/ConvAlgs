d727cbeda90914ff0376d95a657481ffd267f057
PASQUALE MAINOLFI <pm@MacBook-Pro-PM.local>
2023-11-16 23:36:09 +0100

    add outp, inp

--- convalgs/src/convalgs.rs
+++ convalgs/src/convalgs.rs
@@ -1,4 +1,5 @@
 use num_traits::Float;
+use std::ops::{Add, Mul, Div, Sub, AddAssign};
 
 
 pub enum ConvolutionMethod {
@@ -10,34 +11,51 @@ pub enum ConvolutionMethod {
     Ntt
 }
 
-pub struct Convolve<'a, T: Float>
-where
-    T: Float 
-{
+pub struct Convolve<'a, T: Float> {
     x: &'a Vec<T>,
     h: &'a Vec<T>,
 }
 
-impl<'a, T> Convolve<'a, T>
-where 
-    T: Float 
+impl<'a, T> Convolve<'a, T> 
+where
+    T: Float
 {
     pub fn new(x: &'a Vec<T>, h: &'a Vec<T>) -> Self {
-        Self { x, h, }
+        Self { x, h }
     }
 
-    pub fn convolve(&self, mode: ConvolutionMethod) -> Vec<T> {
-        let mut y: Vec<T> = Vec::new();
+    pub fn convolve(&self, mode: ConvolutionMethod) -> Vec<T> 
+    where
+        T: Float + Add + Mul + Div + Sub + AddAssign
+    {
+        let xlen = self.x.len();
+        let hlen = self.h.len();
+        let ylen = xlen + hlen - 1;
+        let mut y: Vec<T> = vec![T::zero(); ylen];
+
         match mode {
-            ConvolutionMethod::InputSide => { todo!() },
-            ConvolutionMethod::OutputSide => { todo!() },
+            ConvolutionMethod::InputSide => {
+                for k in 0..hlen {
+                    for i in 0..xlen {
+                        y[k + i] += self.h[k] * self.x[i]
+                    }
+                }
+            },
+            ConvolutionMethod::OutputSide => {
+                for (n, value) in y.iter_mut().enumerate() {
+                    let lower = 0.max(n as isize - xlen as isize + 1) as usize;
+                    let upper = n.min(hlen - 1);
+                    for k in lower..upper {
+                        *value += self.h[k] * self.x[n - k];
+                    }
+                }
+            },
             ConvolutionMethod::Karatsuba => { todo!() },
             ConvolutionMethod::Fft => { todo!() },
             ConvolutionMethod::OlaFft => { todo!() },
             ConvolutionMethod::Ntt => { todo!() },
         }
+        y
     }
-
-
 
 }
--- convalgs/src/main.rs
+++ convalgs/src/main.rs
@@ -1,7 +1,13 @@
 pub mod convalgs;
+use convalgs::{Convolve, ConvolutionMethod};
 
 
 fn main() {
-    println!("Hello, world!");
+    let x: Vec<f32> = [0.1, -3.2, 1.5, 7.0, 5.7].to_vec();
+    let h: Vec<f32> = [0.1, -3.2, 1.5].to_vec();
+
+    let conv = Convolve::new(&x, &h);
+    let convolution = conv.convolve(ConvolutionMethod::OutputSide);
+    println!("{:?}", convolution);
 }
 
