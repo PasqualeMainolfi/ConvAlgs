3cf2a8ce079b9714fbcc60ad16e9a775582ebc6c
PASQUALE MAINOLFI <pm@MacBook-Pro-PM.local>
2023-11-17 23:05:46 +0100

    add karatsuba

--- convalgs/src/convalgs.rs
+++ convalgs/src/convalgs.rs
@@ -1,5 +1,6 @@
 use num_traits::Float;
-use std::ops::{Add, Mul, Div, Sub, AddAssign};
+use std::ops::{Add, Mul, AddAssign};
+use std::fmt::Debug;
 
 
 pub enum ConvolutionMethod {
@@ -18,44 +19,92 @@ pub struct Convolve<'a, T: Float> {
 
 impl<'a, T> Convolve<'a, T> 
 where
-    T: Float
+    T: Debug + Float + AddAssign + Add + Mul
 {
     pub fn new(x: &'a Vec<T>, h: &'a Vec<T>) -> Self {
         Self { x, h }
     }
 
-    pub fn convolve(&self, mode: ConvolutionMethod) -> Vec<T> 
-    where
-        T: Float + Add + Mul + Div + Sub + AddAssign
-    {
+    pub fn convolve(&self, mode: ConvolutionMethod) -> Vec<T> {
         let xlen = self.x.len();
         let hlen = self.h.len();
         let ylen = xlen + hlen - 1;
         let mut y: Vec<T> = vec![T::zero(); ylen];
 
         match mode {
-            ConvolutionMethod::InputSide => {
-                for k in 0..hlen {
-                    for i in 0..xlen {
-                        y[k + i] += self.h[k] * self.x[i]
-                    }
-                }
+            ConvolutionMethod::InputSide => self._input_side_helper(self.x, self.h, &mut y),
+            ConvolutionMethod::OutputSide => self._output_side_helper(self.x, self.h, &mut y),
+            ConvolutionMethod::Karatsuba => {
+                let size = 1 << (ylen as f32).log2().ceil() as usize;
+                let mut xpad = vec![T::zero(); size];
+                let mut hpad = vec![T::zero(); size];
+                xpad[..xlen].copy_from_slice(&self.x[..]);
+                hpad[..hlen].copy_from_slice(&self.h[..]);
+                let _y = self._karatsuba_helper(&xpad, &hpad);
+                y[..].copy_from_slice(&_y[..ylen]);
             },
-            ConvolutionMethod::OutputSide => {
-                for (n, value) in y.iter_mut().enumerate() {
-                    let lower = 0.max(n as isize - xlen as isize + 1) as usize;
-                    let upper = n.min(hlen - 1);
-                    for k in lower..upper {
-                        *value += self.h[k] * self.x[n - k];
-                    }
-                }
-            },
-            ConvolutionMethod::Karatsuba => { todo!() },
             ConvolutionMethod::Fft => { todo!() },
             ConvolutionMethod::OlaFft => { todo!() },
             ConvolutionMethod::Ntt => { todo!() },
         }
         y
+    }
+
+    fn _input_side_helper(&self, x: &[T], h: &[T], buffer: &mut [T]) {
+        for k in 0..h.len() {
+            for i in 0..x.len() {
+                buffer[k + i] += h[k] * x[i]
+            }
+        }
+    }
+    
+    fn _output_side_helper(&self, x: &[T], h: &[T], buffer: &mut [T]) {
+        for (n, value) in buffer.iter_mut().enumerate() {
+            let lower = 0.max(n as isize - x.len() as isize + 1) as usize;
+            let upper = n.min(h.len() - 1);
+            for k in lower..=upper {
+                *value += h[k] * x[n - k];
+            }
+        }
+    }
+
+    fn _karatsuba_helper(&self, x: &Vec<T>, h: &Vec<T>) -> Vec<T> {
+        let xlen = x.len();
+        let hlen = h.len();
+
+        let n = xlen.max(hlen);
+
+        if n == 1 { 
+            let y = [x[0] * h[0]].to_vec();
+            return y
+        }
+
+        let m = n / 2;
+        let (x0, x1) = x.split_at(m);
+        let (h0, h1) = h.split_at(m);
+
+        let z0 = self._karatsuba_helper(&x0.to_vec(), &h0.to_vec());
+        let z2 = self._karatsuba_helper(&x1.to_vec(), &h1.to_vec());
+
+        let xsum = x0.iter().zip(x1.iter()).map(|(&a, &b)| a + b).collect::<Vec<_>>();
+        let hsum = h0.iter().zip(h1.iter()).map(|(&a, &b)| a + b).collect::<Vec<_>>();
+
+        let z1 = self._karatsuba_helper(&xsum, &hsum);
+        let z1 = z1.iter().zip(z2.iter().zip(z0.iter())).map(|(&a, (&b, &c))| a - b - c).collect::<Vec<_>>();
+        
+        let mut y = vec![T::zero(); 2 * n - 1];
+        for i in 0..z0.len() {
+            y[i] += z0[i]
+        }
+        for i in 0..z1.len() {
+            y[i + m] += z1[i]
+        }
+        for i in 0..z2.len() {
+            y[i + 2 * m] += z2[i]
+        }
+        
+        y
+
     }
 
 }
--- convalgs/src/main.rs
+++ convalgs/src/main.rs
@@ -3,11 +3,17 @@ use convalgs::{Convolve, ConvolutionMethod};
 
 
 fn main() {
+
     let x: Vec<f32> = [0.1, -3.2, 1.5, 7.0, 5.7].to_vec();
-    let h: Vec<f32> = [0.1, -3.2, 1.5].to_vec();
+    let h: Vec<f32> = [0.7, -3.4, 1.0].to_vec();
 
     let conv = Convolve::new(&x, &h);
-    let convolution = conv.convolve(ConvolutionMethod::OutputSide);
-    println!("{:?}", convolution);
-}
+    let inpside = conv.convolve(ConvolutionMethod::InputSide);
+    let outside = conv.convolve(ConvolutionMethod::OutputSide);
+    let karat = conv.convolve(ConvolutionMethod::Karatsuba);
 
+    println!("INPSIDE: {:?}", inpside);
+    println!("OUTSIDE: {:?}", outside);
+    println!("KARAT: {:?}", karat);
+
+}
