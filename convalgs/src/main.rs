f2a0bc9f77890ee7f61aeda7b64ecf26c75baf9d
PASQUALE MAINOLFI <pm@MacBook-Pro-PM.local>
2023-11-19 01:00:57 +0100

    add adn fix in ola fft

--- convalgs/src/main.rs
+++ convalgs/src/main.rs
@@ -4,7 +4,7 @@ use convalgs::{Convolve, ConvolutionMethod};
 
 fn main() {
 
-    let x: Vec<f32> = [0.1, -3.2, 1.5, 7.0, 5.7].to_vec();
+    let x: Vec<f32> = [0.1, -3.2, 1.5, 7.0, 5.7, 8.0, 9.3, 12.7].to_vec();
     let h: Vec<f32> = [0.7, -3.4, 1.0].to_vec();
 
     let conv = Convolve::new(&x, &h);
@@ -12,10 +12,12 @@ fn main() {
     let outside = conv.convolve(ConvolutionMethod::OutputSide);
     let karat = conv.convolve(ConvolutionMethod::Karatsuba);
     let fft = conv.convolve(ConvolutionMethod::Fft);
+    let olafft = conv.convolve(ConvolutionMethod::OlaFft(2));
 
     println!("INPSIDE: {:?}", inpside);
     println!("OUTSIDE: {:?}", outside);
     println!("KARAT: {:?}", karat);
     println!("FFT: {:?}", fft);
+    println!("OLA FFT: {:?}", olafft);
 
 }
